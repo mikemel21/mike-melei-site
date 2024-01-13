6c79f65ec0bc147c18d038a5b4bdef93c1753d7c
mikemel21 <mikemelei905@gmail.com>
2024-01-13 18:46:09 -0500

    removed vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import './style.css';
 import App from './App.vue';
 import { myRouter } from './PageRouter';
 
-import { createVuetify } from 'vuetify';
-import 'vuetify/styles';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
+// import { createVuetify } from 'vuetify';
+// import 'vuetify/styles';
+// import * as components from 'vuetify/components';
+// import * as directives from 'vuetify/directives';
 
-const vuetify = createVuetify({ components, directives });
-createApp(App).use(myRouter).use(vuetify).mount('#app');
+// const vuetify = createVuetify({ components, directives });
+createApp(App).use(myRouter).mount('#app');
