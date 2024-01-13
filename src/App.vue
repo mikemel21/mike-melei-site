33ed0022ab33d8e95903cb4f8b385f4b3fd47ffe
mikemel21 <mikemelei905@gmail.com>
2024-01-13 18:44:44 -0500

    added web code

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,64 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div id="app-area">
+    <!-- Color Schemes to Try:
+    Scheme 1 -> #0E0B16 (Void)   #A239CA (Fuschia)  #4717F6 (Jewel) #E7DFDD (Stark)
+    Scheme 2 -> #000000 (Black)  #062F4F (Ink)  #813772 (Posy)  #B82601 (Embers)
+    https://www.canva.com/learn/website-color-schemes/
+    -->
+    <nav class="nav-bar">
+      <router-link to="/">Home</router-link>
+      <router-link to="/About">About Me</router-link>
+      <router-link to="/Projects">Projects</router-link>
+      <router-link to="/Contact">Contact Me</router-link>
+    </nav>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <main>
+      <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  #app-area {
+      height: 100vh;
+      /* background-color: rgb(64, 64, 64); */
+      font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+          sans-serif;
+  }
+  router-view {
+    overflow: auto;
+  }
+  a {
+    color: #E7DFDD;
+    font-size: 20px;
+    margin: 8px;
+  }
+  .nav-bar {
+    background-color: #A239CA;
+    border-radius: 4px;
+    padding: 5px;
+    width: 100vw;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  
+  /* Fade Transition */
+  /* .fade-leave-active {
+    transition-property: all;
+    transition-duration: 1000ms;
+    transition-timing-function: ease;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .fade-leave {
+    transform: translateY(50%);
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+  .fade-leave-to {
+    transform: translateY(100%);
+    background: green;
+  } */
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { myRouter } from './PageRouter';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const vuetify = createVuetify({ components, directives });
+createApp(App).use(myRouter).use(vuetify).mount('#app');
